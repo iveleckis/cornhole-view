00e583ea2349b6ba249284ca9985b97147d2b165
Ignas Veleckis <Ignas.Veleckis@ibm.com>
2024-08-29 22:45:00 +0300

    clenup

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,7 @@ module.exports = {
     port: 3000,
     open: true,
     hot: true,
-    // compress: true,
-    // historyApiFallback: true,
+    compress: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
